e4d37606fe097b815d3c3dfe674b08e44109cc66
poldiee <nessidave@gmail.com>
2022-05-04 19:32:28 +0300

    api resp

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 # app = Flask(__name__)
-from .config import Config
+from config import Config
 
 app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(Config)
